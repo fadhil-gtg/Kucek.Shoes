f136f9f7497378e964dc54fea8ac2fe0a2d8fa41
Adhika Fadhil <adhikafdhil02@gmail.com>
2026-06-22 01:27:59 +0700

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://kucekshoes.vercel.app',
+  integrations: [
+    tailwind(),
+    sitemap()
+  ],
   output: 'static',
   devToolbar: {
     enabled: false,
