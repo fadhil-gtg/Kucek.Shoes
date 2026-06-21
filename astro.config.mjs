a053ad2b422efb51b62d9b8558b429e0a72954cc
Adhika Fadhil <adhikafdhil02@gmail.com>
2026-06-22 01:19:13 +0700

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://fadhil-gtg.github.io',
-  base: '/Kucek.Shoes',
   integrations: [tailwind()],
   output: 'static',
   devToolbar: {
